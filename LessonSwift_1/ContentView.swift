ec0c78aa2e738f3b89e1d49144df75fca68a872a
Akipon <superakip1995@gmail.com>
2020-08-20 00:26:40 +0900

    初期化

---
+++ LessonSwift_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LessonSwift_1
+//
+//  Created by 高野暉大 on 2020/08/20.
+//  Copyright © 2020 高野暉大. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
